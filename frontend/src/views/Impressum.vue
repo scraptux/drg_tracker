4ddfd258aeb9be5f2d1381805a2340313e6086c4
scraptux <git@thomasjasny.de>
2023-02-09 12:45:13 +0100

    add impressum content

--- frontend/src/views/Impressum.vue
+++ frontend/src/views/Impressum.vue
@@ -5,7 +5,31 @@
   </div>
   <div class="mx-auto max-w-2xl px-4 py-5 sm:px-6 lg:max-w-7xl lg:px-8 border-y mb-4 border-gray-200">
     <p class="mt-1 text-sm text-slate-500">
-      Impressums-Inhalt
+      <span class="text-base text-slate-900">Kontakt</span><br>
+      Thomas Jasny<br>
+      <br><br>
+    </p>
+    <p class="mt-1 text-sm text-slate-500">
+      <span class="text-base text-slate-900">Quelle</span> <br>
+      Die auf dieser Webseite dargestellten Daten basieren auf den herausgegebenen amtlichen Ausgaben der jeweiligen Kodierungssysteme durch das Bundesinstitut für Arzneimittel und Medizinprodukte (BfArM).
+      Die Erstellung erfolgt unter Verwendung der maschinenlesbaren Fassung des Bundesinstituts für Arzneimittel und Medizinprodukte (BfArM).
+      Bei den vom BfArM herausgegebenen amtlichen Ausgaben der ICD-10-GM, Systematisches Verzeichnis, handelt es sich um ein "anderes amtliches Werk" i. S. des § 5 Abs. 2 Urheberrechtsgesetz (UrhG).
+      Nach Urheberrecht/Nutzungsumfang § 1 Abs. 4 des ICD-10-GM- beziehungsweise des OPS-Kodierungssystems ist der Erwerber der Daten berechtigt, vom Format abgeleitete Mehrwertprodukte zu fertigen und zu vertreiben.
+      <br><br>
+    </p>
+    <p class="mt-1 text-sm text-slate-500">
+      <span class="font-medium text-base text-slate-900">Haftungsausschluss</span><br>
+      <span class="text-base text-slate-900">Haftung für Inhalte</span><br>
+      Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt.
+      Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen.
+      Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich.
+      Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.
+      Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt.
+      Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich.
+      Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
+      <br><br>
+      <span class="text-base text-slate-900">Urheberrecht</span><br>
+      Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.
     </p>
   </div>
 
