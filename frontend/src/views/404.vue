e74d6a503b9456d61e09949118fe6b254aaa370a
Thomas Jasny <jasny@stud.uni-frankfurt.de>
2024-02-10 21:37:37 +0100

    add 404 page

---
+++ frontend/src/views/404.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="text-center text-4xl pt-32 text-gray-900 font-medium">
+    E40.4
+  </div>
+  <div class="text-center text-2xl text-gray-900 text-sm">
+    Resource wurde nicht gefunden
+  </div>
+</template>
