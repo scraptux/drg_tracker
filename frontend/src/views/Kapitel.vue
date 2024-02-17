475f0aac037141742e212e3365659f2d35147527
Thomas Jasny <jasny@stud.uni-frankfurt.de>
2024-02-17 20:02:50 +0100

    improve table on small devices

--- frontend/src/views/Kapitel.vue
+++ frontend/src/views/Kapitel.vue
@@ -33,18 +33,18 @@
       </div>
       
       <div class="mx-auto max-w-7xl space-x-2 py-5 sm:px-6 lg:px-3 border-y mb-4 border-gray-200">
-        <dl>
-          <router-link :to="`/${$route.params.drg}/version/${year}/gruppe/${gruppe.GrVon}`"
+        <table class="w-full table-auto">
+          <tr @click="router.push(`/${$route.params.drg}/version/${year}/gruppe/${gruppe.GrVon}`)"
               :class="(index % 2) ? 'bg-white' : 'bg-gray-50'"
-              class="py-5 grid grid-cols-10 gap-4 px-6 hover:bg-gray-200"
+              class="hover:bg-gray-200 hover:cursor-pointer"
               v-for="(gruppe, index) in gruppen" :key="gruppe.GrVon">
-            <dt class="text-sm my-auto font-medium text-gray-500">{{ gruppe.GrVon }} - {{ gruppe.GrBis }}</dt>
-            <dd class="text-sm my-auto text-gray-900 col-span-8">{{ gruppe.GrTi }}</dd>
-            <dd class="my-auto text-gray-500 col-span-1">
+            <td class="py-4 px-6 text-sm text-center my-auto font-medium text-gray-500">{{ gruppe.GrVon }} - {{ gruppe.GrBis }}</td>
+            <td class="py-4 w-full text-sm my-auto text-gray-900">{{ gruppe.GrTi }}</td>
+            <td class="py-4 px-6 my-auto text-gray-500">
               <svg class="float-right w-5 h-5 fill-current" width="200px" height="200px" viewBox="-19.04 0 75.804 75.804" xmlns="http://www.w3.org/2000/svg" fill="#000000" stroke="#000000" stroke-width="0"><g transform="translate(-831.568 -384.448)"> <path d="M833.068,460.252a1.5,1.5,0,0,1-1.061-2.561l33.557-33.56a2.53,2.53,0,0,0,0-3.564l-33.557-33.558a1.5,1.5,0,0,1,2.122-2.121l33.556,33.558a5.53,5.53,0,0,1,0,7.807l-33.557,33.56A1.5,1.5,0,0,1,833.068,460.252Z" fill="#000000"></path> </g></svg>
-            </dd>
-          </router-link>
-        </dl>
+            </td>
+          </tr>
+        </table>
       </div>
     </div>
     <LoadingAnimation v-if="loading!=0 && !error"></LoadingAnimation>
@@ -54,6 +54,7 @@
 
 <script>
 import axios from 'axios'
+import router from '@/router'
 
 import LoadingAnimation from '@/components/LoadingAnimation.vue'
 import ErrorMessage from '@/components/ErrorMessage.vue';
@@ -61,6 +62,7 @@ import ErrorMessage from '@/components/ErrorMessage.vue';
 export default {
   data() {
     return {
+      router: router,
       year: null,
       kapitel: {},
       gruppen: [],
